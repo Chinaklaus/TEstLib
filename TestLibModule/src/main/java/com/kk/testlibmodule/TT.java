2a40248a27511c75c0f737332a0fedd1014dec3b
klaus <136736265@qq.com>
2022-02-21 10:53:18 +0800

    TT.class commit

---
+++ TestLibModule/src/main/java/com/kk/testlibmodule/TT.java
@@ -0,0 +1,14 @@
+package com.kk.testlibmodule;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author Yin.MingHao
+ * @date 2022-02-21
+ */
+public class TT {
+    public void Toast(Context context){
+        Toast.makeText(context, "111", Toast.LENGTH_SHORT).show();
+    }
+}
