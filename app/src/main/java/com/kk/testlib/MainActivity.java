9769d5962d839fdc66489d879af33cb35333469d
klaus <136736265@qq.com>
2022-02-21 14:19:24 +0800

    TT.class commit

--- app/src/main/java/com/kk/testlib/MainActivity.java
+++ app/src/main/java/com/kk/testlib/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kk.testlibmodule.TT;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TT tt = new TT();
+        tt.toast(this);
     }
 }
